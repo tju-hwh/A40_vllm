4ae625381f7f55952df7a6102dc60b9de71afb71
tju-hwh <1007287849@qq.com>
2026-03-23 18:01:38 +0800

    1

--- vllm/v1/engine/llm_engine.py
+++ vllm/v1/engine/llm_engine.py
@@ -255,7 +255,7 @@ class LLMEngine:
             self.engine_core.add_request(child_request)
 
     def step(self) -> Union[list[RequestOutput], list[PoolingRequestOutput]]:
-        print(f"v1 step")
+        # print(f"v1 step")
         if self.should_execute_dummy_batch:
             self.should_execute_dummy_batch = False
             self.engine_core.execute_dummy_batch()
